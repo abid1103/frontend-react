93e8db2371931e43eedb56621d336e5764e8597b
Muhammad Abid Hussain <144558738+abid1103@users.noreply.github.com>
2025-12-14 23:09:53 +0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,9 @@
+// In frontend src/pages/index.jsx or index.tsx
+import { useEffect } from "react";
 
+export default function Home() {
+  useEffect(() => {
+    window.location.href = "https://backend-fastapi-mz7v.onrender.com";
+  }, []);
+  return <div>Redirecting...</div>;
+}
